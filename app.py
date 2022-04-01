50a56d689cc089a26302c452c960f82b69db22d4
Jeremy Kropa <arkedi487@gmail.com>
2022-04-01 10:18:44 -0400

    Made the database connected

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
+
 from flask import Flask,render_template,request,redirect,url_for
 #  this is all of the needed parts for the data base all that needs done is makeing it work
 # And don't forget to download this so that it works
 # pip install Flask psycopg2-binary
-import Database
-
-
 username = 'admin'
 pswrd = 'welcome1'
 app = Flask(__name__)
-userpass = [['admin','password']]
 
 @app.route('/welcome/<name>')
 def welcome(name):
@@ -27,17 +24,38 @@ def login():
     if request.method == 'POST':
         user = request.form['nm']
         userpassed = request.form['pw']
-        if [user,userpassed] in userpass:
-            return redirect(url_for('welcome',name = user))
-        else:
-            return redirect(url_for('error',errType='Incorect Password'))
+        return redirect(url_for('welcome',name = user))
+    else:
+        return redirect(url_for('error',errType='Incorect Password'))
 
 @app.route('/register')
 def regi():
     return render_template('regestration.html')
 
 @app.route('/register',methods=['POST', 'GET'])
+def index():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['Email']
+        user = request.form['unr']
+        password = request.form['pwr']
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute('SELECT * FROM bicycle')
+        result = cur.fetchall()
+        print(result)
+        cur.execute('INSERT INTO bicycle (id, name, email, username, password, bicycleName, bicyclePrice)'
+        'VALUES(%s, %s, %s, %s, %s, %s, %s)',
+        ('10001', name,  email , user , password , 'Bike1' , '$12,000'))
+        conn.commit()
+        cur.close()
+        conn.close()
+        return render_template('login.html')
+    else:
+        return render_template('login.html')
+    
 def registerz():
+
     if request.method == 'POST':
         user = request.form['unr']
         pass1 = request.form['pwr']
@@ -49,11 +67,18 @@ def registerz():
     else:
         user = request.args.get('nm')
         userpassed = request.args.get('pw')
-        if [user,userpassed] in userpass:
-            return redirect(url_for('welcome',name = user))
+        if user == userpassed:
+            return render_template('welcome.html')
         else:
             return redirect(url_for('error', errType ='Account already exists'))
 
+import os
+import psycopg2
+def get_db_connection():
+    conn = psycopg2.connect(
+    "dbname=flask_db user=postgres host=localhost password=Meegee12"
+)
+    return conn
 
 if __name__ == '__main__':
     app.run(debug=True)
